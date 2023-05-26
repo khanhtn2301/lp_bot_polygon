4b6406d64b4b70253135c9fd4395db4646c35e94
khanhtn2301 <khanh.tn200326@sis.hust.edu.vn>
2023-05-26 21:03:12 +0700

    add config etl

---
+++ etl/config.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Imports
+# =============================================================================
+import pandas as pd
+from decimal import Decimal
+
+
+UNISWAP_V3_POLYGON_ENDPOINT = 'https://api.thegraph.com/subgraphs/name/messari/uniswap-v3-polygon'
+
+def pools_v3_dict(token0=None, token1=None, fee_tier=None):
+    pools_df = pd.read_csv('data/raw_data/uniswap_v3/pools.csv')
+    pools_dict_arr = pools_df.to_dict(orient='records')
+    if token0 is not None and token1 is not None and fee_tier is not None:
+        pools_dict_arr = [dict_el for dict_el in pools_dict_arr if
+                          dict_el['token0_symbol'] == token0 and dict_el['token1_symbol'] == token1 and
+                          dict_el['feeTier'] == fee_tier]
+    pools_dict_arr = sorted(pools_dict_arr, key=lambda x: float(x['volumeUSD']), reverse=True)
+
+    return pools_dict_arr
+
+THEGRAPH_QUERY_UNI_V3_POLY_SWAPS = """
+            query swapsQuery($id: String!, $timestamp_start: Int!){
+                liquidityPool( id: $id 
+                ) {
+                    swaps(
+                    first: 1000
+                    orderBy: timestamp
+                    orderDirection: asc
+                    where: { timestamp_gte: $timestamp_start }
+                    ) {
+                    id
+                    timestamp
+                    tokenIn {
+                        id
+                        symbol
+                        name
+                        decimal
+                        lastPriceUSD
+                    }
+                    tokenOut {
+                        id
+                        symbol
+                        name
+                        decimal
+                        lastPriceUSD
+                    }
+                    amountIn
+                    amountOut
+                    amountInUSD
+                    tick
+                    }
+                }
+            }
+        """
+
+def swaps_v3_polygon_dict(data_el):
+    data_dict = {
+        'id': data_el['id'],
+        'timestamp': data_el['timestamp'],
+        'tokenIn_id': data_el['tokenIn']['id'],
+        'tokenIn_symbol': data_el['tokenIn']['symbol'],
+        'tokenIn_lastPriceUSD': float(data_el['tokenIn']['lastPriceUSD']),
+        'tokenOut_id': data_el['tokenOut']['id'],
+        'tokenOut_symbol': data_el['tokenOut']['symbol'],
+        'tokenOut_lastPriceUSD': float(data_el['tokenOut']['lastPriceUSD']),
+        'amountIn': float(data_el['amountIn']),
+        'amountOut': float(data_el['amountOut']),
+        'amountUSD': float(data_el['amountInUSD']),
+        'tick': int(data_el['tick']),
+    }
+    return data_dict
+
+THEGRAPH_QUERY_UNI_V3_POLY_MINTS = """
+            query mintsQuery($id: String!, $timestamp_start: Int!){
+                liquidityPool(id: $id) {
+                    withdraws(
+                    first: 1000
+                    orderBy: timestamp
+                    orderDirection: asc
+                    where: { timestamp_gte: $timestamp_start }
+                    ) {
+                    id
+                    timestamp
+                    inputTokens {
+                        id
+                        symbol
+                        name
+                        lastPriceUSD
+                    }
+                    amountUSD
+                    inputTokenAmounts
+                    tickUpper
+                    tickLower
+                    account {
+                            id
+                        }
+                    }
+                }
+            }
+        """
+
+def mints_v3_polygon_dict(data_el):
+    data_dict = {
+        'id': data_el['id'],
+        'transaction_id': data_el['position']['id'],
+        'owner': data_el['account']['id'],
+        'timestamp': data_el['timestamp'],
+        'token0_id': data_el['inputTokens'][0]['id'],
+        'token0_symbol': data_el['inputTokens'][0]['symbol'],
+        'token0_priceUSD': float(data_el['inputTokens'][0]['lastPriceUSD']),
+        'token1_id': data_el['inputTokens'][1]['id'],
+        'token1_symbol': data_el['inputTokens'][1]['symbol'],
+        'token1_priceUSD': float(data_el['inputTokens'][1]['lastPriceUSD']),
+        'amount0': float(data_el['inputTokenAmounts'][0]),
+        'amount1': float(data_el['inputTokenAmounts'][1]),
+        'amountUSD': float(data_el['amountUSD']),
+        'tickLower': int(data_el['tickLower']),
+        'tickUpper': int(data_el['tickUpper'])
+    }
+
+    return data_dict
+
+THEGRAPH_QUERY_UNI_V3_POLY_BURNS = """
+            query mintsQuery($id: String!, $timestamp_start: Int!){
+                liquidityPool(id: $id) {
+                    withdraws(
+                    first: 1000
+                    orderBy: timestamp
+                    orderDirection: asc
+                    where: { timestamp_gte: $timestamp_start }
+                    ) {
+                    id
+                    timestamp
+                    inputTokens {
+                        id
+                        symbol
+                        name
+                        lastPriceUSD
+                    }
+                    amountUSD
+                    inputTokenAmounts
+                    tickUpper
+                    tickLower
+                    account {
+                            id
+                        }
+                    }
+                }
+            }
+        """
+
+def burns_v3_polygon_dict(data_el):
+    data_dict = {
+        'id': data_el['id'],
+        'transaction_id': data_el['position']['id'],
+        'owner': data_el['account']['id'],
+        'timestamp': data_el['timestamp'],
+        'token0_id': data_el['inputTokens'][0]['id'],
+        'token0_symbol': data_el['inputTokens'][0]['symbol'],
+        'token0_priceUSD': float(data_el['inputTokens'][0]['lastPriceUSD']),
+        'token1_id': data_el['inputTokens'][1]['id'],
+        'token1_symbol': data_el['inputTokens'][1]['symbol'],
+        'token1_priceUSD': float(data_el['inputTokens'][1]['lastPriceUSD']),
+        'amount0': float(data_el['inputTokenAmounts'][0]),
+        'amount1': float(data_el['inputTokenAmounts'][1]),
+        'amountUSD': float(data_el['amountUSD']),
+        'tickLower': int(data_el['tickLower']),
+        'tickUpper': int(data_el['tickUpper'])
+    }
+
+    return data_dict
